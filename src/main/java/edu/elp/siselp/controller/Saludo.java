9f42c7d6730773985d5b53b9f22aa7b62911642e
Jham3s <elisson19nov@gmail.com>
2022-04-26 11:53:16 -0500

    Subiendo proyecto siselp2

---
+++ src/main/java/edu/elp/siselp/controller/Saludo.java
@@ -0,0 +1,42 @@
+package edu.elp.siselp.controller;
+
+import edu.elp.siselp.entity.Persona;
+import edu.elp.siselp.service.IPersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("index")
+public class Saludo {
+
+    @Autowired
+    private IPersonaService personaService;
+
+    @GetMapping("/listaPersonas")
+    public List<Persona> listaPersona(){
+        return this.personaService.listaPersonas();
+    }
+
+    @GetMapping("/buscarPersona")
+    public Persona buscarPersona(@RequestParam("dni") String dni){
+        return this.personaService.obtenerPersonaByDni(dni);
+    }
+
+
+    @GetMapping("/saludo")
+    public String saludo(@RequestParam String nombre){
+        return "Hola Mundo desde Ayacucho!!!!!.... Te saluda" + nombre;
+    }
+
+    @GetMapping("/bienvenida")
+    public String bienvenida(){
+        return "Hola amig@s al curso de Java";
+    }
+
+
+}
